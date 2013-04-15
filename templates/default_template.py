40c343551268012e5c0202f50f485d983a07a3ba
Luca Postregna <luca.postregna@gmail.com>
2013-04-15 12:28:01 +0200

    fix

--- templates/default_template.py
+++ templates/default_template.py
@@ -187,6 +187,16 @@ def footer(node):
 		</div>	
 	  	<script src="'''+ PREFIX +'''js/jquery.js"></script> 
   		<script src="'''+ PREFIX +'''js/hashgrid.js"></script>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-6164762-13']);
+  			_gaq.push(['_trackPageview']);
+  			(function() {
+    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  			})();
+		</script>	
 	</body>
 </html>'''
     return html	
